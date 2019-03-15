184af09cd355e6e96a64fa534519870cca8d335c
odrum428 <oxkeita@gmail.com>
2019-03-16 03:44:53 +0900

    改行データも保持するように修正

--- make_markov_chain.py
+++ make_markov_chain.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import csv
 from janome.tokenizer import Tokenizer
+import random
+from collections import defaultdict # キーが存在しなくても辞書データを更新できる
 
 """
 与えられた文書からマルコフ連鎖のための辞書データを作成するためのプログラム
 """
 
 t = Tokenizer()
+text = ""
 
 # Janomeを使用してテキストデータを単語に分割する
 def wakati(text):
-    text = text.replace('\n', '')  # 改行を削除
     text = text.replace('\u3000', '')  # 全角の空白を削除
     t = Tokenizer()
     result = t.tokenize(text, wakati=True)
