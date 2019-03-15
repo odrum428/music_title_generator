1c5b70c04390c434199dde04aaac962ba8464094
odrum428 <oxkeita@gmail.com>
2019-03-16 02:43:36 +0900

    Janomeを使ってタイトルをそれぞれ形態素解析させてみた

--- make_markov_chain.py
+++ make_markov_chain.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import csv
+from janome.tokenizer import Tokenizer
 
 """
 与えられた文書からマルコフ連鎖のための辞書データを作成するためのプログラム
 """
 
+t = Tokenizer()
+
 # ファイルの読み込み
 with open('./music_title_list_from_karatetsu.csv') as f:
-    print(f.read())
+    for token in t.tokenize(f.read()):
+        print(token)
